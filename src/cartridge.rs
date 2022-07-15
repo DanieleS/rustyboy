f5f6a179e7c497cc343f6cabe426d16a7693f007
Davide Specchia <davide.specchia@mirta.com>
2022-07-15 11:49:12 +0200

    Implement basic MBC1

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -1,9 +1,17 @@
 use anyhow::Result;
 use std::{fs::File, io::Read};
 
+const MBC_TYPE_ADDRESS: usize = 0x147;
+
+#[derive(Clone, Debug)]
+struct Mbc1State {
+    selected_rom_bank: u8,
+}
+
 #[derive(Clone, Debug)]
 enum Mbc {
     NoMbc,
+    Mbc1(Mbc1State),
 }
 
 #[derive(Clone, Debug)]
@@ -34,12 +42,9 @@ impl Cartridge {
 
         let title = Cartridge::parse_title(&data)?;
         let header = CartridgeHeader { title };
+        let mbc = Cartridge::parse_mbc(&data)?;
 
-        Ok(Cartridge {
-            header,
-            data,
-            mbc: Mbc::NoMbc,
-        })
+        Ok(Cartridge { header, data, mbc })
     }
 
     fn is_new_cartridge(data: &[u8]) -> bool {
@@ -57,18 +62,45 @@ impl Cartridge {
             .map_err(|_| anyhow::anyhow!("Invalid cartridge title"))?;
         Ok(title.trim_end_matches('\0').to_string())
     }
+
+    fn parse_mbc(data: &[u8]) -> Result<Mbc> {
+        let mbc_type = data[MBC_TYPE_ADDRESS];
+        let mbc_state = match mbc_type {
+            0x00 => Mbc::NoMbc,
+            0x01 => Mbc::Mbc1(Mbc1State {
+                selected_rom_bank: 1,
+            }),
+            _ => return Err(anyhow::anyhow!("Invalid cartridge MBC type")),
+        };
+        Ok(mbc_state)
+    }
 }
 
 impl Cartridge {
+    #[inline(always)]
     pub fn read(&self, address: u16) -> u8 {
-        match self.mbc {
+        match &self.mbc {
             Mbc::NoMbc => self.data[address as usize],
+            Mbc::Mbc1(state) => match address {
+                0x0000..=0x3fff => self.data[address as usize],
+                _ => {
+                    self.data[address as usize + ((state.selected_rom_bank as usize) - 1) * 0x4000]
+                }
+            },
         }
     }
 
+    #[inline(always)]
     pub fn write(&mut self, address: u16, value: u8) {
-        match self.mbc {
+        match &mut self.mbc {
             Mbc::NoMbc => (),
+            Mbc::Mbc1(state) => match address {
+                0x2000..=0x3fff => {
+                    let value = if value == 0 { 1 } else { value & 0x1f };
+                    state.selected_rom_bank = value;
+                }
+                _ => (),
+            },
         }
     }
 }
